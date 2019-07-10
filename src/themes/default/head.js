583f16a5fa660d33a99fe0b05e7275c2979925ea
cristian-g <cristian@cristiangonzalez.com>
2019-07-11 00:26:32 +0200

    Title updated

--- src/themes/default/head.js
+++ src/themes/default/head.js
@@ -1,12 +1,12 @@
 export default {
   title: 'Default Theme',
-  titleTemplate: '%s - Vue Storefront',
+  titleTemplate: '%s - Wisconsin Jeans',
   htmlAttrs: {
     lang: 'en'
   },
   meta: [
     { charset: 'utf-8' },
-    { vmid: 'description', name: 'description', content: 'Vue Storefront is a standalone PWA storefront for your eCommerce, possible to connect with any eCommerce backend (eg. Magento, Prestashop or Shopware) through the API.' },
+    { vmid: 'description', name: 'description', content: 'Wisconsin Jeans' },
     { name: 'viewport', content: 'width=device-width, initial-scale=1, minimal-ui' },
     { name: 'robots', content: 'index, follow' },
     { name: 'mobile-web-app-capable', content: 'yes' },
